aeb83e228c8ed4f3deea927c298d8d0950540a3b
jasonkatsoutas <jasonskate33@gmail.com>
2021-07-31 19:11:47 +0300

    vega lite modal configuration some styles

--- src/components/common/SelectAggregate.tsx
+++ src/components/common/SelectAggregate.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from "react";
 import {FormControl, InputLabel, makeStyles, Select, SelectProps} from "@material-ui/core";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles(() => ({
     option: {
@@ -14,9 +15,21 @@ const useStyles = makeStyles(() => ({
     },
     select: {
         minWidth: '150px',
-        width:'100%'
-
-    }
+        padding: '4px 8px',
+        width: '100% !important',
+        boxShadow: '0 1px 4px 2px #E8E8E8',
+        border: '1px solid #C0C0C0',
+        borderRadius: 4,
+        transition: '0.2s',
+        background: 'white',
+        '&:hover': {
+            background: 'whitesmoke',
+        },
+        '& :focus': {
+            background: 'none'
+        }
+    },
+    formControl: {}
 }))
 
 type selectAggregateProps = {
@@ -26,9 +39,20 @@ type selectAggregateProps = {
 const SelectAggregate: FC<SelectProps & selectAggregateProps> = ({selectTitle, children, ...props}) => {
     const classes = useStyles()
 
-    return <FormControl>
-        {selectTitle?<InputLabel htmlFor={'aggregate-select'}>{selectTitle}</InputLabel>:undefined}
-        <Select id={'aggregate-select'} className={classes.select} {...props}>
+    return <FormControl className={classes.formControl}>
+        {selectTitle ? <InputLabel htmlFor={'aggregate-select'}>{selectTitle}</InputLabel> : undefined}
+        <Select MenuProps={{
+            anchorOrigin: {
+                vertical: "bottom",
+                horizontal: "left"
+            },
+            transformOrigin: {
+                vertical: "top",
+                horizontal: "left"
+            },
+            getContentAnchorEl: null
+        }} disableUnderline={true} IconComponent={ExpandMoreIcon} id={'aggregate-select'}
+                className={classes.select} {...props}>
             {children}
         </Select>
     </FormControl>

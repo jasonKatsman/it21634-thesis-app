f5dcaa855f66402f5b842cffb1aa6a61252fa7c0
jasonkatsoutas <jasonskate33@gmail.com>
2021-10-05 11:37:07 +0300

    mobile view

--- src/components/vega/VegaLitePreview.tsx
+++ src/components/vega/VegaLitePreview.tsx
@@ -3,22 +3,29 @@ import {BoxProps, makeStyles} from "@material-ui/core";
 import vegaEmbed from "vega-embed"
 
 const useStyles = makeStyles(() => ({
-    list: {
-        maxHeight: 500,
-        overflow: 'auto'
-    },
-    listItem: {
-        cursor: 'pointer',
-        '& > :nth-child(n)': {
-            pointerEvents: 'none'
+    root: {
+        '&.vega-embed.has-actions': {
+            padding: 0
+        },
+        '& summary': {
+            border: '10px solid red',
+            background: 'red',
+            opacity: '0.2 !important',
+            transform: 'scale(0.7)',
+            '&:hover': {
+                opacity: '0.5 !important',
+                transform: 'scale(0.7)'
+            },
+
         }
     }
 }))
 type VegaComponentProps = {
+    className?: any
     vegaConfig: any
     keyId: string
 }
-const VegaLitePreview: FC<VegaComponentProps & BoxProps> = ({vegaConfig, keyId = 'idkey', ...props}) => {
+const VegaLitePreview: FC<VegaComponentProps & BoxProps> = ({className, vegaConfig, keyId = 'idkey', ...props}) => {
     const classes = useStyles();
 
     useEffect(() => {
@@ -27,7 +34,7 @@ const VegaLitePreview: FC<VegaComponentProps & BoxProps> = ({vegaConfig, keyId =
 
 
     return (
-        <div id={keyId} {...props}/>
+        <div className={`${classes.root} ${className}`} id={keyId} {...props}/>
     );
 }
 
